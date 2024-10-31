955a5f3668b327640a90eac052dfdf8dead75c5f
azzy72 <s215045@dtu.dk>
2024-10-31 10:05:54 +0100

    Pathing added to constants.py

--- constants.py
+++ constants.py
@@ -1,3 +1,7 @@
 import os
 cwd = os.getcwd() + "\\"
 datadir = cwd + "data\\"
+
+#Dataset from string
+ProteinInfo_path = "https://stringdb-downloads.org/download/protein.info.v12.0/9606.protein.info.v12.0.txt.gz"
+ProteinEnrichment_path = "https://stringdb-downloads.org/download/protein.enrichment.terms.v12.0/9606.protein.enrichment.terms.v12.0.txt.gz"
