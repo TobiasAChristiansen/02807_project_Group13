f9f2eb53e95029673f607c3f3223848205cf11bf
Yayi0117 <joycewang1403@gmail.com>
2024-11-06 10:19:39 +0100

    define density function

--- functions.py
+++ functions.py
@@ -6,3 +6,25 @@ def swapkeyval(indict):
     for k, v in indict.items():
         outdict[str(v)] = k
     return outdict
+
+
+def density(cluster):
+    """
+    Calculate density of the entire graph
+    For a cluster C with n nodes and m edges, density is defined as:
+    Density(C) = 2m/n(n-1)
+    """
+    num_edges = 0
+    num_nodes = len(cluster)
+    
+    # Count all edges
+    for node, neighbors in cluster.items():
+        num_edges += len(neighbors)
+    
+    num_edges //= 2
+    
+    if num_nodes < 2:
+        return 0
+    
+    density = (2 * num_edges) / (num_nodes * (num_nodes - 1))
+    return density
