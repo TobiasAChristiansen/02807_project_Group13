9462cddf101440f700d51b8d7f5ac361a43136de
TobiasAChristiansen <s215105@student.dtu.dk>
2024-11-15 11:26:52 +0100

    Shortest path

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 import networkx as nx
+import time
 
 def swapkeyval(indict):
     """
@@ -45,41 +46,47 @@ def check_connection(cluster):
         return False, clusters
     
 
-def shortest_path(vertex, all_vertices):
+def shortest_path(vertex, all_vertices, debug_mode=False, timed = False):
     # Setting up a dictionary of shortest paths and the start vertex is put in a list
     shortest_paths = dict()
     to_process = [str(vertex)]
+    count = 0
 
     # While the "to_process" list is not empty, we do branch and bound
     while to_process:
-        current_branch = to_process.pop()
+        if debug_mode:
+            print(count, len(to_process), len(shortest_paths))
+        
+        #Taking one of the short branches to process
+        current_branch = to_process.pop(0)
 
         # We look through all neighbors. If it goes to a vertex already in the path, it's unoptimal and is discarded
         for neighbor in all_vertices[int(current_branch.split("_")[-1])]:
-            if str(neighbor) in current_branch.split("_"):
-                continue  # Skip if neighbor is already in the current branch
-
+            
             # If the path doesn't loop, we add the neighbor to the current branch and check if it's a new shortest path
             new_path = current_branch + "_" + str(neighbor)
-            to_process.append(new_path)
-            start_end = current_branch.split("_")[0] + "->" + str(neighbor)
+            start_end = new_path.split("_")[0] + "->" + str(neighbor)
             
             if start_end in shortest_paths:
                 # If there are more paths of equal length, we're working with a list
                 if isinstance(shortest_paths[start_end], list):
                     if len(new_path.split("_")) < len(shortest_paths[start_end][0].split("_")):
                         shortest_paths[start_end] = new_path
+                        to_process.append(new_path)
                     elif len(new_path.split("_")) == len(shortest_paths[start_end][0].split("_")):
                         shortest_paths[start_end].append(new_path)
+                        to_process.append(new_path)
                 else:
                     # If there's only one shortest path found until now
                     if len(new_path.split("_")) < len(shortest_paths[start_end].split("_")):
                         shortest_paths[start_end] = new_path
+                        to_process.append(new_path)
                     elif len(new_path.split("_")) == len(shortest_paths[start_end].split("_")):
                         shortest_paths[start_end] = [shortest_paths[start_end], new_path]
+                        to_process.append(new_path)
             else:
                 # If no shortest path has been identified between the two points
                 shortest_paths[start_end] = new_path
+                to_process.append(new_path)
         
-    # Returning the output
     return shortest_paths
